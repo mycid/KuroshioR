37f744f27aa6ea730813007853c9c7a905580e1c
Trevor Eakes <mycid@uw.edu>
2016-01-17 21:30:02 -0800

    A new branch representing a the needed changes for neutral theory.
    This branch is likely to become the master if no changes prove to be
    deleterious. Refer to this branch over the master from the time being.

--- KuroshioAnalysisPipeline.R
+++ KuroshioAnalysisPipeline.R
@@ -1,6 +1,4 @@
 #Kuroshio Analysis Pipline#
-  
-setwd("/home/trevor/Desktop/2009KuroshioData/KuroshioR")
 folder <- getwd() #sets directory as an object/path to folder that holds multiple .csv files
 file_list <- list.files(path=folder, pattern="*.csv") # create list of all .csv files in folder
 # read in each .csv file in file_list and create a data frame with the same name as the .csv file
@@ -10,20 +8,21 @@ for (i in 1:length(file_list)){
   )} #Downloading all csv files to the Global Environment
 
 #Loading packages
-packages <- c("akima", "geosphere", "ggplot2", "gridExtra", "plyr", "prabclus", "sparcl", "vegan")
-library("akima", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("geosphere", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("ggplot2", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("plyr", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("gridExtra", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("prabclus", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("sparcl", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("vegan", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("iNEXT", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("rareNMtests", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("RColorBrewer", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("grid", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
-library("gplots", lib.loc="/home/trevor/R/i686-pc-linux-gnu-library/3.2")
+packages <- c("akima","iNEXT", "rareNMtests", "RColorBrewer", "grid", "gplots", "geosphere", "ggplot2", "gridExtra", "plyr", "prabclus", "sparcl", "vegan")
+library("akima", lib.loc="~/R/win-library/3.2")
+library("colorspace", lib.loc="~/R/win-library/3.2")
+library("geosphere", lib.loc="~/R/win-library/3.2")
+library("ggplot2", lib.loc="~/R/win-library/3.2")
+library("gplots", lib.loc="~/R/win-library/3.2")
+library("gridExtra", lib.loc="~/R/win-library/3.2")
+library("iNEXT", lib.loc="~/R/win-library/3.2")
+library("plyr", lib.loc="~/R/win-library/3.2")
+library("prabclus", lib.loc="~/R/win-library/3.2")
+library("RColorBrewer", lib.loc="~/R/win-library/3.2")
+library("sparcl", lib.loc="~/R/win-library/3.2")
+library("vegan", lib.loc="~/R/win-library/3.2")
+library("untb", lib.loc="~/R/win-library/3.2")
+library("GUILDS", lib.loc="~/R/win-library/3.2")
 #Preparing the main dataset
 test <- Kuroshio_Phytoplankton.csv #shortens name
 DiversityIndex <- Create.Diversity.I(test, 3,length(test)) #Makes a new dataframe with diversity indexes 
@@ -40,10 +39,8 @@ div.abiotic <- PotentialST(d,t,s ) #function for calculating the potential densi
 #Expanding the data set; more things to analyze
 bigtest <- Allbigstuff.csv #changing the datset to an easier name
 #|This data set has the generalized large classes of micro plankton found in the samples. It has a total cells per liter for all cells
-TotA <-apply(bigtest[, 3:9], 1, function(x) sum(x)) #This is all cells for all microplankton
-Transect <-Untitled3.csv #This file created to add the categorial transect data
-Transect <- rbind("A", Transect) #A fix for Transect, adds a missing row
-Adiv.abiotic <- cbind(Transect, TotA, div.abiotic) #creates a new master dataset with additional information
+TotA <-data.frame(apply(bigtest[, 3:9], 1, function(x) sum(x))) #This is all cells for all microplankton
+Adiv.abiotic <- cbind(bigtest, TotA, div.abiotic) #creates a new master dataset with additional information
 Adiv.abiotic <- div.abiotic[-c(186:190),] #removes the last 5 rows because they have missing data
 
 #Preparing Chlorophyll data
@@ -62,7 +59,6 @@ Adiv.abiotic <- cbind(Chlorophyll, Fluorescence, Adiv.abiotic) #adds Chlorophyll
 #Clustering data based on abiotic factors
 Abiotic <- cbind(Adiv.abiotic$S, Adiv.abiotic$Theta ) #Creates the desired dataframe with all the abiotic factors to be clustered
 AbioticCluster(Abiotic, 3) #Creates three different method dendrograms and three seperate clade lists based on each of those methods
-
 
 #names of the cladedata sets it saves are: 
 #1. Gccom which is the "complete" method
@@ -138,10 +134,14 @@ Kuroshio_Phytoplankton <- cbind(clades, Kuroshio_Phytoplankton)
 Kuroshio_Genus <- cbind(clades, Kuroshio_Genus)
 #Creating a list for rarefaction based on the clusters
 #AbundInf <- as.numeric(apply(test[1:185, 3:ncol(test)], 2, function(x) sum(x>0)))
-SplitData(Kuroshio_Phytoplankton, 2, "dfcluster") #Splits Adiv.abiotic into multiple data frames based on the clusters 
+Arc2 <- Arc.csv[,-1] #Removes one factor from the arc dataset so that it can be clustered. 
+#Can use either Arc2 or Kuroshio_Phytoplankton
+SplitData(Arc2, 1, "dfcluster") #Splits Adiv.abiotic into multiple data frames based on the clusters 
+SplitData(Kuroshio_Phytoplankton, 2, 'dfcluster')
 libr <- list(dfcluster1, dfcluster2,dfcluster3) #,dfcluster4,dfcluster5) #Combines into one big list for the purpose of using a function on it to do everything at once
 
 #turns the seperate datasets into incidence data then removes zeros, finally adding them together. There should be a function for this. 
+#for Kuroshio
 clus1 <- as.numeric(apply(dfcluster1[,4:ncol(dfcluster1)], 2, function(x) sum(x>0)))
 clus1 <- c(nrow(dfcluster1), clus1[clus1>0])
 clus2 <- as.numeric(apply(dfcluster2[,4:ncol(dfcluster2)], 2, function(x) sum(x>0)))
@@ -153,8 +153,8 @@ clus3 <- c(nrow(dfcluster3), clus3[clus3>0])
 #clus5 <- as.numeric(apply(dfcluster5[,4:ncol(dfcluster5)], 2, function(x) sum(x>0)))
 #clus5 <- c(nrow(dfcluster5), clus5[clus5>0])
 Rarefaction <- list("1"=clus1, "2"=clus2, "3"=clus3)# "5"=clus5) 
-
-z <- iNEXT(Rarefaction, q=0, datatype="incidence", se=TRUE) #z is object class iNEXT. It provides all the data shown in the rarefaction graphs
+#--------------------------------------
+z <- iNEXT(Rarefaction, q=0,  datatype="incidence", se=TRUE) #z is object class iNEXT. It provides all the data shown in the rarefaction graphs
 ggiNEXT(z, type = 1, se = TRUE, facet.var = "order", color.var = "order") #Creates rarefaction graphs
 #--------------------------------------------------------------------------
 #
@@ -165,7 +165,6 @@ Incidence <- t(cbind(clus1, clus2, clus3))
 Kuroshio_Phytoplankton <- Kuroshio_Phytoplankton.csv[-c(186:190), -c(1,2)]
 colnames(Incidence)<-colnames(Kuroshio_Phytoplankton)
 Incidence <- as.matrix(Incidence)
-
 #
 IncidenceMatrix <- heatmap(t(Incidence), Rowv=NA, Colv=NA, scale="row",margins=c(10,5))
 #
@@ -213,7 +212,7 @@ plot(Celltest)
 kruskal.test(Richness~S, data=Adiv.abioticC)
 MeanCell <- c(mean(Adiv.cluster1$Cellcount), mean(Adiv.cluster2$Cellcount), mean(Adiv.cluster3$Cellcount, na.rm=TRUE))
 SDCell <- c(sd(Adiv.cluster1$Cellcount), sd(Adiv.cluster2$Cellcount), sd(Adiv.cluster3$Cellcount, na.rm=TRUE))
-summary(aov(Richness~, data=Adiv.abiotic))
+summary(aov(Richness~Chlorophyll, data=Adiv.abiotic))
 #
 #Number of Species which appear once or twice for each cluster
 Kurolets <- sum(clus1[which(clus1==2 | clus1==1)])
@@ -222,17 +221,17 @@ Oyalets <- sum(clus3[which(clus3==2 | clus3==1)])
 
 ####################################################
 Euclid <- test[-c(186:190), c(3:length(test))]
-Adiv.abioticC <- Adiv.abiotic[-c(which(rowSums(Euclid!=0)==FALSE)),]
-Euclid <- Euclid[rowSums(Euclid)!=0, ] 
+#Adiv.abioticC <- Adiv.abiotic[-c(which(rowSums(Euclid!=0)==FALSE))]
+#Euclid <- Euclid[rowSums(Euclid)!=0, ] 
 Euclid[Euclid>0]<- 1 
-Euclid <- cbind(Adiv.abioticC$lon, Euclid)
+Euclid <- cbind(Adiv.abiotic$lon, Euclid)
 SplitData(Euclid, 1, "EuclidLon")
-SplitData(Adiv.abioticC, 15, "Transect")
-dA <- vegdist(EuclidLon143.5, na.rm=TRUE)
-dB <- vegdist(EuclidLon144, na.rm=TRUE)
-dC <- vegdist(EuclidLon144.5, na.rm=TRUE)
-dD<- vegdist(EuclidLon145, na.rm=TRUE)
-dE <- vegdist(EuclidLon145.5, na.rm=TRUE)
+SplitData(Adiv.abiotic, 15, "Transect")
+dA <- vegdist(EuclidLon143.5, method="jaccard")
+dB <- vegdist(EuclidLon144, method="jaccard", na.rm=TRUE)
+dC <- vegdist(EuclidLon144.5, method="jaccard", na.rm=TRUE)
+dD<- vegdist(EuclidLon145, method="jaccard", na.rm=TRUE)
+dE <- vegdist(EuclidLon145.5, method="jaccard", na.rm=TRUE)
 
 #Euclid[Euclid>0]<- 1 
 #d <- vegdist(Euclid, na.rm=TRUE) # distance matrix
@@ -255,14 +254,186 @@ TransectC <- cbind(Transect144.5, WardC)
 TransectD <- cbind(Transect145, WardD)
 TransectE <- cbind(Transect145.5, WardE)
 
+ColorDendrogram(wardE, y = WardE, labels = names(WardE), main = "Transect E (with Abund)", branchlength = 1.5)
+NPfactorInColor(TransectE, xvar="DfromF", yvar= "depth..m.", WardE, xlab="DfromF", ylab="depth..m.", title="Transect E (with Abund)") 
+NPfactorInColor(TransectE, xvar="S", yvar= "Theta", WardE,xlab="Sal", ylab="Theta", title="Transect E (with Abund)")
+###################################################
+#Comparing the distance matrices
+dA <- as.matrix(dA)
+Alist <- as.list(dA)
+Adist <- as.list(dist1)
+plot(Alist, Adist)
+#
+dB <- as.matrix(dB)
+Blist <- as.list(db)
+Bdist <- as.list(dist2)
+#
+dC <- as.matrix(dC)
+Clist <- as.list(dC)
+Cdist <- as.list(dist3)
+#
+dD <- as.matrix(dD)
+Dlist <- as.list(dD)
+Ddist <- as.list(dist4)
+#
+dE <- as.matrix(dE)
+Elist <- as.list(dE)
+Edist <- as.list(dist5)
+#
+#All Distance Matrix comparison
+Euclid <- test[-c(186:190), c(3:length(test))]
+Adiv.abioticC <- Adiv.abiotic[-c(which(rowSums(Euclid!=0)==FALSE)),]
+Alldist <- cbind(Adiv.abioticC$lon, Adiv.abioticC$lat)
+distAll <- distm(Alldist)
+distAll <- as.matrix(distAll)
+Alllist <- as.list(distAll)
+#This plots the total community dissimilarity and 
+Euclid <- Euclid[rowSums(Euclid)!=0, ] 
+Euclid[Euclid>0]<- 1 
+BioticDist <- vegdist(Euclid, method="jaccard", na.rm=TRUE)
+Allbio <- hclust(BioticDist, method="ward.D")
+Allclust <- cutree(Allbio, 5)
+ColorDendrogram(Allbio, y = Allclust, labels = names(Allclust), main = "Transect C (with Abund)", branchlength = 1.5)
+BioticDist <- as.matrix(BioticDist)
+AllBiolist <- as.list(BioticDist)
+mantel(AllBioDist, distAll, method="pearson", permutations=999)
+bandplot(Alllist, AllBiolist)
+#trying to figure out rank abundance stuff below. Not working yet. 
+rankabuncomp(x,y="",factor,scale="abundance",scaledx=F,type="o",rainbow=T,
+             legend=T,xlim=c(1,max1), ylim=c(0,max2), ...)
+rankabuncomp(Kuroshio_Phytoplankton[4:74], scaledx="abundance", rainbow = T, legend=T)
+#-----------------------------------------------------
+#below is a script for creating a species correlation matrx in heat map style
+#It covers everything from organizing the dataset to graphing it and adding numbers
+bigmatrix <- as.matrix(round(cor(test[,3:length(test)], method="pearson"), 3))
+cormat <- reorder_cormat(bigmatrix)
+bigmatrix <- get_upper_tri(cormat)
+upper_tri <- as.data.frame(bigmatrix)
+melted_cormat <- melt(bigmatrix)
+melted_cormat <- na.omit(melted_cormat)
 
-ColorDendrogram(wardC, y = WardC, labels = names(WardC), main = "Transect C (with Abund)", branchlength = 1.5)
+# Create a ggheatmap
+ggheatmap <- ggplot(melted_cormat, aes(X2, X1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed()
 
-NPfactorInColor(TransectC, xvar="DfromF", yvar= "depth..m.", WardC, xlab="DfromF", ylab="depth..m.", title="Transect C (with Abund)") 
+# Print the heatmap
+print(ggheatmap)
+#
+ggheatmap + 
+  geom_text(aes(X2, X1, label = value), color = "black", size = 4) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(0.6, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "top", title.hjust = 0.5))
+#-----------------------------------------------------------
 
-NPfactorInColor(TransectC, xvar="S", yvar= "Theta", WardC, xlab="Sal", ylab="Theta", title="Transect C (with Abund)") 
+DiaOver <- (bigtest$Diatoms/bigtest$Dinoflagellates)
+DiaOver <- DiaOver[-c(186:190)]
+Adiv.abiotic<- cbind(Adiv.abiotic, DiaOver="DiaOverDino")
+EU <- ggplot(Adiv.abiotic[which("depth..m."==0),], aes(x =lat, y =depth..m., colour=DiaOverDino, label=round(DiaOverDino,2)))+ geom_point(size=7, alpha=.9, label=c, position=position_dodge(), stat="identity", )+scale_color_gradientn(colours=jet.colors(7), space="rgb", guide="colourbar")
+EU <- EU+geom_text(aes(label=round(DiaOverDino,2)),hjust=0, vjust=-.5)
+EU <- EU+labs(x="Salinity", y="Theta")+stat_contour(z=Adiv.abiotic$sigPoDen, binwidth = 2)
+EU <- EU+theme(axis.title.x = element_text(color="cadet blue", vjust=-0.35, size=20, face="bold"), axis.title.y = element_text(color="cadetblue" , vjust=0.35, size=20, face="bold"))
+CF <- EU+theme(axis.text.x=element_text(size=20, vjust=0.5), axis.text.y=element_text(size=20, vjust=.05))
+CF #Temporary
+Adiv.abiotic["depth..m."==0]
+##---------------------------------------------------------
+species.count(t(test[,c(3:length(test))]))
+plot(species.count(t(test[,c(3:length(test))])),type="b")
+matplot(species.table(t(test[,c(3:length(test))])),type="l",lty=1)
+optimal.params.sloss(t(test[,c(3:73)]))
+optimal.params.sloss(t(test[,c(3:73)]), nbres = 10, ci = TRUE, cint = c(0.025, 0.975))
+#Examples
+a <- untb(start=rep(1,50), prob=0.01, gens=2000, keep=TRUE)
+plot(species.count(a),type="c")
+matplot(species.table(a),type="l",lty=1)
+##
+PrestonR <- count(colSums(test[,c(3:length(test))]))
+Preston<- preston(PrestonR, 16)*.1
+summary.count(PrestonR)
+optimal.prob(PrestonR)
+plot(PrestonR)
+plot.preston(Preston)
+#Changing cell volumes to centi litters. A,B,C transects are .08, D and E are .1
+Cent <- test[-c(186:190), c(3:length(test))]
+Cent <- cbind(Adiv.abiotic$lon, Cent)
+SplitData(Cent, 1, "centi")
+centitest <- rbind(centi143.5[2:ncol(centi143.5)]/80, centi144[2:ncol(centi144)]/80, centi144.5[2:ncol(centi144.5)]/80, centi145[2:ncol(centi145)]/100, centi145.5[2:ncol(centi145.5)]/100)
+centitest <- cbind(clades, centitest)
+SplitData(centitest, 2, 'dfcluster')
+
+#finding the optimal theta and m coefficients for all three water mass clusters
+#Also setting up PrestonPlots for each
+#Kuroshio
+Kuroshio <- dfcluster1[,-c(1:3)]
+Kuroshio <- Kuroshio[,which(colSums(Kuroshio)>0)]
+KuroSum <- apply(Kuroshio, 2, function(x) sum(x))#sumrows
+PresK <- count(KuroSum)
+PrestonK<-preston(PresK)
+plot.preston(PresK, main="Kuroshio")
+plot.preston(PrestonK, main="Kuroshio")
+#Oyashio
+Oyashio <- dfcluster3[,-c(1:4)]
+Oyashio <- Oyashio[,which(colSums(Oyashio)>0)]
+OyaSum <- apply(Oyashio, 2, function(x) sum(x))#sumrows
+PresO <- count(OyaSum)
+PrestonO<-preston(PresO)
+plot.preston(PresO, main="Oyashio")
+plot.preston(PrestonO, main="Oyashio")
+#Deep
+Deep <- dfcluster2[,-c(1:4)]
+Deep <- Deep[,which(colSums(Deep)>0)]
+DeepSum <- apply(Deep, 2, function(x) sum(x))#sumrows
+PresD <- count(DeepSum)
+PrestonD<-preston(PresD)
+plot.preston(PresD, main="Deep")
+plot.preston(PrestonD, main="Deep")
+plot()
+#Forming a main dataset to estimate parameters for each one of these
+####
+
+optimal.theta(PresK, N=100)
+optimal.params.sloss(t(Kuroshio), nbres=10)
+optimal.params.gst(t(Kuroshio), exact = TRUE, ci = FALSE, cint = c(0.025, 0.975), nbres = 100)
+#
+l <- logkda.R(phytoInt[2,], use.brob=TRUE)  # Use logkda() if pari/gp is available
+
+T <- optimal.params(phytoInt[1,], log.kda=l)  #compare his answer of 7.047958 and 0.22635923.
+T1<- optimal.params(phytoInt[2,], log.kda=l)  #compare his answer of 7.047958 and 0.22635923.
 
 
-Celltest <- aov(WardA~ShannonWiener, data=Transect143.5)
-summary(Celltest)
- 
+#
+KuroSim <- replicate(10, rand.neutral(no.of.ind(PresK),optimal.theta(PresK), string=("spp.")))
+#
+plot(PresK)
+replicate(100, lines(rand.neutral(no.of.ind(PresK),optimal.theta(PresK), string=("spp.")), type="S", col="grey"))
+#
+Lines <- as.table(preston(rand.neutral(no.of.ind(PresK),optimal.theta(PresK), string=("spp."))))
+
+par(mar=c(5.1,4.1,4.1,5.1)) #this must be before both plots are made
+plot(PrestonK)
+par(new=TRUE)
+plot(Lines, axes=FALSE, xlab="", ylab="", type="b",lty=2, pch=24, col="black", bg=125)
+axis(1, at=1:5, lab=c("Mon","Tue","Wed","Thu","Fri"))
+mtext("Salinity",side=4,col="red",line=3) 
+axis(4, ylim=c(33.6,34.3), col="blue" ,col.axis="blue",las=1)
+legend("bottomleft",legend=c("S Gradient/Adjusted Distance","Salinity/Station Distance"), cex=.75,
+       text.col=c("green","blue"),pch=c(23,24),col=c("green","blue"))
+
+
+plot.count(PrestonR, uncertainty = TRUE, expectation = TRUE, theta = 6.810761, n = 100)
+rand.neutral(no.of.ind(PrestonR), optimal.theta(PrestonR),string="NULL")
